9d66088fa70b4679252ba8e0a6fb46761701220f
maksim <smstepani4@gmail.com>
2020-09-05 02:59:21 +0300

    initial commit

---
+++ routes/channels.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+|
+| Here you may register all of the event broadcasting channels that your
+| application supports. The given channel authorization callbacks are
+| used to check if an authenticated user can listen to the channel.
+|
+*/
+
+/*Broadcast::channel('App.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});*/
+
+
+Broadcast::channel('room.{room_id}', function ($user, $room_id) {
+    //return true;
+    //return (int) $user->id === (int) $room_id;
+    //return (int) $user->id === 1;
+
+    //return $user->rooms->contains($room_id);
+
+    if ($user->rooms->contains($room_id)){
+        return $user->name;
+    }
+
+});
